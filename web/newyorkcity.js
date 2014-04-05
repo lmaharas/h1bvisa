9aacbec3d907a45e7949ec881195fecabd8152c5
Jia Zhang <jiazhang@Jias-MacBook-Pro.local>
2014-04-05 15:51:40 -0400

    bar graph not stacked

--- web/newyorkcity.js
+++ web/newyorkcity.js
@@ -1,18 +1,3 @@
-/**
- * Created by hen on 3/8/14.
- */
-d3.csv("/reduced_data/h1b_newyork.csv", function(data)
-	{
-		visas = [];
-	//	console.log(data);
-		for(visa in data){
-			visas.push(data[visa])
-		}
-	//	console.log(visas)
-		//aggregateByCountry("CANADA")
-		aggregateBySectorAndStatus("Retail", "Denied")
-	}
-)
 
 function aggregateByCountry(country){
 	//function that organize selected country's visas by sector, then status
@@ -73,7 +58,6 @@ function aggregateBySectorAndStatus(Sector, Status){
 	var visasBySectorStatus = []
 	for (visa in visasTargetSectorStatus){	
 		var currentCountry = visasTargetSectorStatus[visa]["Origin Country"];
-
 		if(visasBySectorStatus[currentCountry]==undefined){
 			visasBySectorStatus[currentCountry]=[]
 			visasBySectorStatus[currentCountry].push(visasTargetSectorStatus[visa])
@@ -81,20 +65,148 @@ function aggregateBySectorAndStatus(Sector, Status){
 		else{
 			visasBySectorStatus[currentCountry].push(visasTargetSectorStatus[visa])
 		}
-	}
-	console.log(visasBySectorStatus["Origin Country"])
-	
+	}	
 	console.log(visasBySectorStatus)
 }
 
-function drawBarGraph(){
 
-	};
+function aggregatebySectorThenStatus(){
+	//function that organizes by each sector and then by each status
+	//format: sector, # certified, # withdrawn, # denied
+	var bySector = 
+		{
+		    "IT":[],
+		    "Other Economic Sector":[],
+		    "Advanced Mfg":[],
+		    "Educational Services":[],
+		    "Finance":[],
+		    "Health Care":[],
+			"undefined":[],
+		    "Aerospace":[],
+		    "Retail":[],
+		    "Hospitality":[],
+		    "Construction":[],
+		    "Energy":[],
+		    "Automotive":[],
+		    "Transportation":[],
+		    "Agribusiness":[],
+		    "Biotechnology":[],
+		    "Geospatial":[],
+		    "Homeland Security":[]
+		}
+	for (visa in visas){
+		//console.log(visasByCountry[visa]["Economic Sector"])
+		var currentSector = visas[visa]["Economic Sector"];
+		var currentStatus = visas[visa]["Status"];
+		if(bySector[currentSector][currentStatus]==undefined){
+			bySector[currentSector][currentStatus]=[]
+			bySector[currentSector][currentStatus].push(visas[visa])
+		}else{
+			bySector[currentSector][currentStatus].push(visas[visa])
+		}
+	}
+	console.log(bySector)
+	return(bySector)
+	
+}
+
+d3.csv("/reduced_data/h1b_newyork.csv", function(data)
+	{
+		visas = [];
+	//	console.log(data);
+		for(visa in data){
+			visas.push(data[visa]);
+		}
+		//aggregateByCountry("CANADA")
+		//aggregateBySectorAndStatus("Retail", "Denied")
+		//aggregatebySectorThenStatus();
+		drawBarGraph(data)
+	}
+)
+
+function drawBarGraph(dataset){
+	var bySector = 
+		{
+		    "IT":[],
+		    "Other Economic Sector":[],
+		    "Advanced Mfg":[],
+		    "Educational Services":[],
+		    "Finance":[],
+		    "Health Care":[],
+			"undefined":[],
+		    "Aerospace":[],
+		    "Retail":[],
+		    "Hospitality":[],
+		    "Construction":[],
+		    "Energy":[],
+		    "Automotive":[],
+		    "Transportation":[],
+		    "Agribusiness":[],
+		    "Biotechnology":[],
+		    "Geospatial":[],
+		    "Homeland Security":[]
+		}
+	for (visa in dataset){
+		//console.log(visasByCountry[visa]["Economic Sector"])
+		var currentSector = visas[visa]["Economic Sector"];
+		var currentStatus = visas[visa]["Status"];
+		if(bySector[currentSector][currentStatus]==undefined){
+			bySector[currentSector][currentStatus]=[]
+			bySector[currentSector][currentStatus].push(visas[visa])
+		}else{
+			bySector[currentSector][currentStatus].push(visas[visa])
+		}
+	}	
+//	console.log(bySector)
+	var sectorStats = []
+	for (sector in bySector){
+//		console.log(bySector[sector])
+		if(bySector[sector]["Certified"]== null){
+			var cSize = 0
+		}else{ 
+			cSize = bySector[sector]["Certified"].length
+		}
+		if(bySector[sector]["Withdrawn"]== null){
+			var wSize = 0
+		}else{ 
+			wSize = bySector[sector]["Withdrawn"].length
+		}
+		if(bySector[sector]["Denied"]== null){
+			var dSize = 0
+		}else{ 
+			dSize = bySector[sector]["Denied"].length
+		}
+
+		sectorStats[sector]=[]
+		sectorStats[sector].push(cSize)
+		sectorStats[sector].push(wSize)
+		sectorStats[sector].push(dSize)
+	}
+	
+	console.log(sectorStats)
+	var w = 400;
+	var h = 600;
+//	console.log("draw bar")
+	var svg = d3.select("body")
+		.append("svg:svg")
+		.attr("width", w)
+		.attr("height", h);
+	svg.selectAll("rect")
+		.data(sectorStats["IT"])
+		.enter()
+		.append("rect")
+		.attr("x", function(d,i){return i*20})
+		.attr("y", 0)
+		.attr("width", 20)
+		.attr("height", function(d,i){
+			return d/2
+		})
+		.attr("fill", function(d,i){d3.rgb(i*30,0,0)});
+};
 
 
 function drawMap() {
-	console.log("map")
+	//console.log("drawMap function")
 }
 
 drawMap();
-drawBarGraph();
